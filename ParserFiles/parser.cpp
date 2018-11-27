7f1002e4f3c342a72e4dbc7ce0d266b9b9b6299f
Nick <nicholas.zimmermann@gmail.com>
2018-11-26 19:45:26 -0800

    committing trash

--- ParserFiles/parser.cpp
+++ ParserFiles/parser.cpp
@@ -15,9 +15,55 @@ using namespace std;
 
 // ----- Utility and Globals -----------------------------------
 
-// ** Need syntaxerror1 and syntaxerror2 functions (each takes 2 args)
-// ** Be sure to put the name of the programmer above each function
-// i.e. Done by:
+//  Need syntax_error1 and syntax_error2 functions (each takes 2 args)
+//  Be sure to put the name of the programmer above each function
+// i.e. Done by: Nick Murrin
+// Global File Type
+fstream toParse;
+fstream Errors;
+// Syntax Errors
+int error_found = 0;
+void syntax_error1(tokentype); // first syntax error function
+void syntax_error2(string);   // second syntax error function
+/* Extra Credit
+//char trace_syntax_error = 'n';
+//char trace_errors_output = 'n';
+//char trace_remove_errors = 'n';
+*/
+// Functions:
+void syntax_error1(tokentype thetype)//when match() function does not match
+{
+  error_found = 1;
+  //output to screen and error.txt file
+  cout << "SYNTAX ERROR: expected " << tokenName[thetype] << " but found " << saved_lexeme << endl;
+
+  if(Errors.is_open())
+    Errors << "SYNTAX ERROR: expected " << tokenName[thetype] << " but found " << saved_lexeme << endl;
+
+  if (trace_syntax_error == 'y')//error checking WIP, never goes here
+    {
+      cout << "Instead of "<< saved_lexeme << " try (eofm to exit): ";
+      cin >> saved_lexeme;
+    }
+  else
+    {
+      Errors << "===================="  << endl;
+      exit(EXIT_FAILURE);//end program
+    }
+}
+//--------------------------------------------------
+void syntax_error2(string parseFunc)//when a switch statement goes to default
+{
+  error_found = 1;
+  //output to screen and error.txt file
+  cout << "SYNTAX ERROR: unexpected " << saved_lexeme << " found in " << parseFunc << endl;
+
+  if(Errors.is_open())
+    Errors << "SYNTAX ERROR: unexpected " << saved_lexeme << " found in " << parseFunc  << endl;
+
+  Errors << "===================="  << endl;
+  exit(EXIT_FAILURE);//always finishes the program
+}
 
 // ** Need the updated match and next_token (with 2 global vars)
 // ** Be sure to put the name of the programmer above each function
