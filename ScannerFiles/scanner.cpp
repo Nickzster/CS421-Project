6ac37866daa746315d4bc5154f4ee48c44580c1c
ClayFlores <ClayFlores@users.noreply.github.com>
2018-11-26 17:21:27 -0800

    adds working parser to clay branch
    removes main from scanner

--- ScannerFiles/scanner.cpp
+++ ScannerFiles/scanner.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "DFA.cpp"
+
 using namespace std;
 
 //=====================================================
@@ -11,6 +11,7 @@ using namespace std;
 // --------- DFAs ---------------------------------
 
 //See DFA.cpp. 
+#include "DFA.cpp"
 
 // ------------ Scaner and Driver ----------------------- 
 
@@ -18,25 +19,72 @@ ifstream fin;  // global stream for reading from the input file
 
 // Scanner processes only one word each time it is called
 // Gives back the token type and the word itself
-// ** Done by: 
+// ** Done by: Nick Zimmermann
+ 
 int scanner(tokentype& a, string& w)
 {
 
   // ** Grab the next word from the file via fin
+  fin >> w;
+  
+  // 2. Call the token functions one after another (if-then-else)
+  if(word(w)) //if it is in the regular DFA
+  {
+    if(w.at(w.size()-1) == 'I' || w.at(w.size()-1) == 'E') //if the word ends in I or E
+    { //then the expression is WORD2
+      a = WORD2;
+    }
+    else //otherwise, it is WORD1
+    { 
+      a = WORD1;
+    }
+    //if it is a reserved word, then overwrite the token type.
+    for(int i = 0; i < reservedWordsList.size(); i++)
+    {
+      reservedWord r = reservedWordsList.at(i);
+      if(r.WORD == w) //if the word is found in the reserved list
+      {
+        a = r.TYPE; //then append its token type
+        break; 
+      }
+    }
+  }
+  else if(period(w)) //if it is in the period DFA
+  {
+    a = PERIOD;
+    // for(int i = 0; i < reservedWordsList.size(); i++)
+    // {
+    //   reservedWord r = reservedWordsList.at(i);
+    //   if(r.WORD == w) //if the word is found in the reserved list
+    //   {
+    //     a = r.TYPE; //then append its token type
+    //     break; 
+    //   }
+    // }
+  }
+  else //if it is not found in any of the word DFA's
+  {
+    a = ERROR;
+  }
 
-  /* 
-  2. Call the token functions one after another (if-then-else)
-     And generate a lexical error message if both DFAs failed.
-     Let the token_type be ERROR in that case.
-  3. Make sure WORDs are checked against the reservedwords list
-     If not reserved, token_type is WORD1 or WORD2.
-  4. Return the token type & string  (pass by reference)
-  */
+  if(a == ERROR)
+  {
+    cout << "***Lexical Error: " << w << " is not a valid token." << "***" << endl;
+  }
+
+    return 1;
+  //    And generate a lexical error message if both DFAs failed.
+  //    Let the token_type be ERROR in that case.
+  // 3. Make sure WORDs are checked against the reservedwords list
+  //    If not reserved, token_type is WORD1 or WORD2.
+  // 4. Return the token type & string  (pass by reference)
+  
+
 
 }//the end of scanner
 
 
-
+/*
 // The temporary test driver to just call the scanner repeatedly  
 // This will go away after this assignment
 // DO NOT CHANGE THIS!!!!!! 
@@ -44,6 +92,7 @@ int scanner(tokentype& a, string& w)
 int main()
 {
   tokentype thetype;
+  init();
   string theword; 
   string filename;
 
@@ -57,13 +106,13 @@ int main()
     {
        scanner(thetype, theword);  // call the scanner
        if (theword == "eofm") break;  // stop now
-
+       cout << "========================================" << endl;
        cout << "Type is:" << tokenName[thetype] << endl;
        cout << "Word is:" << theword << endl;   
+       cout << "========================================" << endl << endl;
     }
 
    cout << "End of file is encountered." << endl;
    fin.close();
 
-}// end
-
+}// end */
