c4461dd779aedfd0e467f01e6360b0b8ab8339a2
ClayFlores <ClayFlores@users.noreply.github.com>
2018-11-26 19:32:12 -0800

    changes to the parser file
    possible syntax error functions with some work on the math and next token as well

--- ParserFiles/parser.cpp
+++ ParserFiles/parser.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+
 using namespace std;
 
+#include "scanner.cpp"
+int scanner(tokentype& a, string& w);
+
+bool word(string s);
+bool period(string s);
+void init();
 // INSTRUCTION:  Complete all ** parts.
 // You may use any method to connect this file to scanner.cpp
 // that you had written.  
@@ -15,14 +22,27 @@ using namespace std;
 
 // globally declared to be available, as per instructions
 string saved_lexeme;
-string saved_token;
+tokentype saved_token;
+bool token_available = false;
 
 // ----- Utility and Globals -----------------------------------
 
 // ** Need syntaxerror1 and syntaxerror2 functions (each takes 2 args)
 // ** Be sure to put the name of the programmer above each function
 // i.e. Done by:
-
+void syntax_error1(tokentype thetype, string saved_lexeme)//when match() function does not match
+{
+	cout << "ERROR: expected " << thetype << " but found " << saved_lexeme <<  endl;
+	cout << "Exiting Program" << endl;
+	exit(EXIT_FAILURE);
+}
+//--------------------------------------------------
+void syntax_error2(string pFunction, string saved_lexeme)//when a switch statement goes to default
+{
+	cout << "ERROR: unexpected " << saved_lexeme << " found in " << pFunction << endl;
+	cout << "Exiting Program" << endl;
+	exit(EXIT_FAILURE);//always ends program
+}
 // ** Need the updated match and next_token (with 2 global vars)
 // ** Be sure to put the name of the programmer above each function
 // next_token(void)
@@ -33,28 +53,30 @@ string saved_token;
 //    A token is grabbed but is not eaten up.
 //    Returns the saved_token
 //   Done by : Clay Flores
-token_type next_token()
+tokentype next_token()
 {
-   if (!token_available)					 // if there is no saved token yet
-     { scanner(saved_token, saved_lexeme);	 // call scanner to grab a new token
-       token_available = true;				 // mark that fact that you have saved it
-     }
-   return saved_token;    // return the saved token
- }
+	if (!token_available)					 // if there is no saved token yet
+	{
+		scanner(saved_token, saved_lexeme);	 // call scanner to grab a new token
+		token_available = true;				 // mark that fact that you have saved it
+	}
+	return saved_token;    // return the saved token
+}
 
 //match(expected)
 //		Checks and eats up the expected token.
 //		HOW: checks to see if expected is different from next_token()
 //		and if so, generates a syntax error and handles the error
 //		else token_available becomes false (eat up) and returns true.
-boolean match(token_type expected)
+bool match(tokentype expected)
 {
 	if (next_token() != expected)  // mismatch has occurred with the next token
 	{ // calls a syntax error function here to  generate a syntax error message here and do recovery
-		syntax_error1(token_type thetype);
+		syntax_error1(expected, saved_lexeme);
 	}
 	else  // match has occurred
 	{
+		cout << "Matched " << expected << endl;
 		token_available = false;  // eat up the token
 		return true;              // say there was a match
 	}
@@ -69,21 +91,22 @@ boolean match(token_type expected)
 // ** Be sure to put the name of the programmer above each function
 // i.e. Done by:
 
-
+ 
 //---------------------------------------
 
 // The new test driver to start the parser
 // Done by:  **
 int main()
 {
-  cout << "Enter the input file name: ";
-  cin >> filename;
-  fin.open(filename.c_str());
-
-  //** calls the <story> to start parsing
-  //** closes the input file 
+	/*
+	cout << "Enter the input file name: ";
+	cin >> filename;
+	fin.open(filename.c_str());
+	*/
+	//** calls the <story> to start parsing
+	//** closes the input file 
 
 }// end
-//** require no other input files!
-//** syntax error EC requires producing errors.text of messages
+ //** require no other input files!
+ //** syntax error EC requires producing errors.text of messages
 
--- ScannerFiles/DFA.cpp
+++ ScannerFiles/DFA.cpp
@@ -5,219 +5,235 @@
 
 using namespace std;
 
+enum tokentype { ERROR, WORD1, WORD2, PERIOD, VERB, VERBNEG, VERBPAST, VERBPASTNEG, IS, WAS, OBJECT, SUBJECT, DESTINATION, PRONOUN, CONNECTOR, EOFM };
+
 // ** MYTOKEN DFA to be replaced by the WORD DFA
 // ** Done by: Clay Flores
-// ** RE:
+// ** RE: 
+// (b|g|h|k|m|n|p|r) (y ((a|e|i|o|u|E|I)^+ | (a|e|i|o|u|E|I)^+ n) | (a|e|i|o|u|E|I)^+ | (a|e|i|o|u|E|I)^+ n)
+// (a|e|i|o|u|E|I)^+ | (a|e|o|i|u|E|I)^+ n
+// (d|j|w|y|z) ( (a|e|i|o|u|E|I)^+ | (a|e|i|o|u|E|I)^+ n)
+// s ( h ( (a|e|i|o|u|E|I)^+ | (a|e|i|o|u|E|I)^+ n) ) | ( (a|e|i|o|u|I|E)^+ |  (a|e|i|o|u|I|E)^+ n) )
+// t ( s ( (a|e|i|o|u|I|E)^+ | (a|e|i|o|u|I|E)^+ n) ) ) | ( (a|e|i|o|u|I|E)^+ | (a|e|i|o|u|I|E)^+ n )
+// c h ( (a|e|i|o|u|I|E)^+ | (a|e|i|o|u|I|E)^+ n)
+
 bool word(string s)
 {
-  int state = 0;
-  int charpos = 0;
+	int state = 0;
+	int charpos = 0;
 
-  while (s[charpos] != '\0') 
-    {
-	// THE STATES ARE AS FOLLOWS:
-	//		   q0  == 0
-	//	   conson  == 1
-	//		 pair  == 2
-	//			s  == 3
-	//			t  == 4
-	//          c  == 5
-	//          q1 == 6
+	while (s[charpos] != '\0')
+	{
+		// THE STATES ARE AS FOLLOWS:
+		//		   q0  == 0
+		//	   conson  == 1
+		//		 pair  == 2
+		//			s  == 3
+		//			t  == 4
+		//          c  == 5
+		//          q1 == 6
 
-  // q0 
-	  // conson = (d|j|w|y)
-	  if (state == 0 && s[charpos] == 'd')
-		  state = 1;
-	  else if (state == 0 && s[charpos] == 'j')
-		  state = 1;
-	  else if (state == 0 && s[charpos] == 'w')
-		  state = 1;
-	  else if (state == 0 && s[charpos] == 'y')
-		  state = 1;
-	  // pair = (b|g|h|k|m|n|p|r)
-	  else if (state == 0 && s[charpos] == 'b')
-		  state = 2;
-	  else if (state == 0 && s[charpos] == 'g')
-		  state = 2;
-	  else if (state == 0 && s[charpos] == 'h')
-		  state = 2;
-	  else if (state == 0 && s[charpos] == 'k')
-		  state = 2;
-	  else if (state == 0 && s[charpos] == 'm')
-		  state = 2;
-	  else if (state == 0 && s[charpos] == 'n')
-		  state = 2;
-	  else if (state == 0 && s[charpos] == 'p')
-		  state = 2;
-	  else if (state == 0 && s[charpos] == 'r')
-		  state = 2;
-	  // s path = s
-	  else if (state == 0 && s[charpos] == 's')
-		  state = 3;
-	  // t path = t
-	  else if (state == 0 && s[charpos] == 't')
-		  state = 4;
-	  // c path = c
-	  else if (state == 0 && s[charpos] == 'c')
-		  state = 5;
-	  // q0' = (a|e|i|o|u|E|I)
-	  else if (state == 0 && s[charpos] == 'a')
-		  state = 6;
-	  else if (state == 0 && s[charpos] == 'e')
-		  state = 6;
-	  else if (state == 0 && s[charpos] == 'i')
-		  state = 6;
-	  else if (state == 0 && s[charpos] == 'E')
-		  state = 6;
-	  else if (state == 0 && s[charpos] == 'I')
-		  state = 6;
-	  else if (state == 0 && s[charpos] == 'o')
-		  state = 6;
-	  else if (state == 0 && s[charpos] == 'u')
-		  state = 6;
-  // conson 
-	  // q0' = (a|e|i|o|u|E|I)
-	  else if (state == 1 && s[charpos] == 'a')
-		  state = 6;
-	  else if (state == 1 && s[charpos] == 'e')
-		  state = 6;
-	  else if (state == 1 && s[charpos] == 'i')
-		  state = 6;
-	  else if (state == 1 && s[charpos] == 'E')
-		  state = 6;
-	  else if (state == 1 && s[charpos] == 'I')
-		  state = 6;
-	  else if (state == 1 && s[charpos] == 'o')
-		  state = 6;
-	  else if (state == 1 && s[charpos] == 'u')
-		  state = 6;
-  // pair
-	  // conson = y
-	  else if (state == 2 && s[charpos] == 'y')
-		  state = 1;
-	  // q0' = (a|e|i|o|u|E|I)
-	  else if (state == 2 && s[charpos] == 'a')
-		  state = 6;
-	  else if (state == 2 && s[charpos] == 'e')
-		  state = 6;
-	  else if (state == 2 && s[charpos] == 'i')
-		  state = 6;
-	  else if (state == 2 && s[charpos] == 'E')
-		  state = 6;
-	  else if (state == 2 && s[charpos] == 'I')
-		  state = 6;
-	  else if (state == 2 && s[charpos] == 'o')
-		  state = 6;
-	  else if (state == 2 && s[charpos] == 'u')
-		  state = 6;
-  // s 
-	  // conson = h
-	  else if (state == 3 && s[charpos] == 'h')
-		  state = 1;
-	  // q0' = (a|e|i|o|u|E|I)
-	  else if (state == 3 && s[charpos] == 'a')
-		  state = 6;
-	  else if (state == 3 && s[charpos] == 'e')
-		  state = 6;
-	  else if (state == 3 && s[charpos] == 'i')
-		  state = 6;
-	  else if (state == 3 && s[charpos] == 'E')
-		  state = 6;
-	  else if (state == 3 && s[charpos] == 'I')
-		  state = 6;
-	  else if (state == 3 && s[charpos] == 'o')
-		  state = 6;
-	  else if (state == 3 && s[charpos] == 'u')
-		  state = 6;
-  // t
-	  // conson = s
-	  else if (state == 4 && s[charpos] == 's')
-		  state = 1;
-	  // q0' = (a|e|i|o|u|E|I)
-	  else if (state == 4 && s[charpos] == 'a')
-		  state = 6;
-	  else if (state == 4 && s[charpos] == 'e')
-		  state = 6;
-	  else if (state == 4 && s[charpos] == 'i')
-		  state = 6;
-	  else if (state == 4 && s[charpos] == 'E')
-		  state = 6;
-	  else if (state == 4 && s[charpos] == 'I')
-		  state = 6;
-	  else if (state == 4 && s[charpos] == 'o')
-		  state = 6;
-	  else if (state == 4 && s[charpos] == 'u')
-		  state = 6;
-  // c
-	// conson = h
-	  else if (state == 5 && s[charpos] == 'h')
-		  state = 1;
-  // q0'
-    // q0' = (a|e|i|o|u|E|I)^*
-	  else if (state == 6 && s[charpos] == 'a')
-		  state = 6;
-	  else if (state == 6 && s[charpos] == 'e')
-		  state = 6;
-	  else if (state == 6 && s[charpos] == 'i')
-		  state = 6;
-	  else if (state == 6 && s[charpos] == 'o')
-		  state = 6;
-	  else if (state == 6 && s[charpos] == 'u')
-		  state = 6;
-	  else if (state == 6 && s[charpos] == 'E')
-		  state = 6;
-	  else if (state == 6 && s[charpos] == 'I')
-		  state = 6;
+		// q0 
+		// conson = (d|j|w|y)
+		if (state == 0 && s[charpos] == 'd')
+			state = 1;
+		else if (state == 0 && s[charpos] == 'j')
+			state = 1;
+		else if (state == 0 && s[charpos] == 'w')
+			state = 1;
+		else if (state == 0 && s[charpos] == 'y')
+			state = 1;
+		// pair = (b|g|h|k|m|n|p|r)
+		else if (state == 0 && s[charpos] == 'b')
+			state = 2;
+		else if (state == 0 && s[charpos] == 'g')
+			state = 2;
+		else if (state == 0 && s[charpos] == 'h')
+			state = 2;
+		else if (state == 0 && s[charpos] == 'k')
+			state = 2;
+		else if (state == 0 && s[charpos] == 'm')
+			state = 2;
+		else if (state == 0 && s[charpos] == 'n')
+			state = 2;
+		else if (state == 0 && s[charpos] == 'p')
+			state = 2;
+		else if (state == 0 && s[charpos] == 'r')
+			state = 2;
+		// s path = s
+		else if (state == 0 && s[charpos] == 's')
+			state = 3;
+		// t path = t
+		else if (state == 0 && s[charpos] == 't')
+			state = 4;
+		// c path = c
+		else if (state == 0 && s[charpos] == 'c')
+			state = 5;
+		// q0' = (a|e|i|o|u|E|I)
+		else if (state == 0 && s[charpos] == 'a')
+			state = 6;
+		else if (state == 0 && s[charpos] == 'e')
+			state = 6;
+		else if (state == 0 && s[charpos] == 'i')
+			state = 6;
+		else if (state == 0 && s[charpos] == 'E')
+			state = 6;
+		else if (state == 0 && s[charpos] == 'I')
+			state = 6;
+		else if (state == 0 && s[charpos] == 'o')
+			state = 6;
+		else if (state == 0 && s[charpos] == 'u')
+			state = 6;
+		// conson 
+		// q0' = (a|e|i|o|u|E|I)
+		else if (state == 1 && s[charpos] == 'a')
+			state = 6;
+		else if (state == 1 && s[charpos] == 'e')
+			state = 6;
+		else if (state == 1 && s[charpos] == 'i')
+			state = 6;
+		else if (state == 1 && s[charpos] == 'E')
+			state = 6;
+		else if (state == 1 && s[charpos] == 'I')
+			state = 6;
+		else if (state == 1 && s[charpos] == 'o')
+			state = 6;
+		else if (state == 1 && s[charpos] == 'u')
+			state = 6;
+		// pair
+		// conson = y
+		else if (state == 2 && s[charpos] == 'y')
+			state = 1;
+		// q0' = (a|e|i|o|u|E|I)
+		else if (state == 2 && s[charpos] == 'a')
+			state = 6;
+		else if (state == 2 && s[charpos] == 'e')
+			state = 6;
+		else if (state == 2 && s[charpos] == 'i')
+			state = 6;
+		else if (state == 2 && s[charpos] == 'E')
+			state = 6;
+		else if (state == 2 && s[charpos] == 'I')
+			state = 6;
+		else if (state == 2 && s[charpos] == 'o')
+			state = 6;
+		else if (state == 2 && s[charpos] == 'u')
+			state = 6;
+		// s 
+		// conson = h
+		else if (state == 3 && s[charpos] == 'h')
+			state = 1;
+		// q0' = (a|e|i|o|u|E|I)
+		else if (state == 3 && s[charpos] == 'a')
+			state = 6;
+		else if (state == 3 && s[charpos] == 'e')
+			state = 6;
+		else if (state == 3 && s[charpos] == 'i')
+			state = 6;
+		else if (state == 3 && s[charpos] == 'E')
+			state = 6;
+		else if (state == 3 && s[charpos] == 'I')
+			state = 6;
+		else if (state == 3 && s[charpos] == 'o')
+			state = 6;
+		else if (state == 3 && s[charpos] == 'u')
+			state = 6;
+		// t
+		// conson = s
+		else if (state == 4 && s[charpos] == 's')
+			state = 1;
+		// q0' = (a|e|i|o|u|E|I)
+		else if (state == 4 && s[charpos] == 'a')
+			state = 6;
+		else if (state == 4 && s[charpos] == 'e')
+			state = 6;
+		else if (state == 4 && s[charpos] == 'i')
+			state = 6;
+		else if (state == 4 && s[charpos] == 'E')
+			state = 6;
+		else if (state == 4 && s[charpos] == 'I')
+			state = 6;
+		else if (state == 4 && s[charpos] == 'o')
+			state = 6;
+		else if (state == 4 && s[charpos] == 'u')
+			state = 6;
+		// c
+		// conson = h
+		else if (state == 5 && s[charpos] == 'h')
+			state = 1;
+		// q0'
+		// q0' = (a|e|i|o|u|E|I)^*
+		else if (state == 6 && s[charpos] == 'a')
+			state = 6;
+		else if (state == 6 && s[charpos] == 'e')
+			state = 6;
+		else if (state == 6 && s[charpos] == 'i')
+			state = 6;
+		else if (state == 6 && s[charpos] == 'o')
+			state = 6;
+		else if (state == 6 && s[charpos] == 'u')
+			state = 6;
+		else if (state == 6 && s[charpos] == 'E')
+			state = 6;
+		else if (state == 6 && s[charpos] == 'I')
+			state = 6;
 
-	// q0 = n
-	  else if (state == 6 && s[charpos] == 'n')
-		  state = 0;
-	// conson = (d|j|w|y|z)
-	  else if (state == 6 && s[charpos] == 'd')
-		  state = 1;
-	  else if (state == 6 && s[charpos] == 'j')
-		  state = 1;
-	  else if (state == 6 && s[charpos] == 'w')
-		  state = 1;
-	  else if (state == 6 && s[charpos] == 'y')
-		  state = 1;
-	  else if (state == 6 && s[charpos] == 'z')
-		  state = 1;
-	// pair = (b|g|h|k|m|n|p|r)
-	  else if (state == 6 && s[charpos] == 'b')
-		  state = 2;
-	  else if (state == 6 && s[charpos] == 'g')
-		  state = 2;
-	  else if (state == 6 && s[charpos] == 'h')
-		  state = 2;
-	  else if (state == 6 && s[charpos] == 'k')
-		  state = 2;
-	  else if (state == 6 && s[charpos] == 'm')
-		  state = 2;
-	  else if (state == 6 && s[charpos] == 'n')
-		  state = 2;
-	  else if (state == 6 && s[charpos] == 'p')
-		  state = 2;
-	  else if (state == 6 && s[charpos] == 'r')
-		  state = 2;
-	// s path = s
-	  else if (state == 6 && s[charpos] == 's')
-		  state = 3;
-	// t path = t
-	  else if (state == 6 && s[charpos] == 't')
-		  state = 4;
-	// c path = c
-	  else if (state == 6 && s[charpos] == 'c')
-		  state = 5;
-      else
-	  return(false);
-      charpos++;
-    }//end of while
+		// q0 = n
+		else if (state == 6 && s[charpos] == 'n')
+			state = 0;
+		// conson = (d|j|w|y|z)
+		else if (state == 6 && s[charpos] == 'd')
+			state = 1;
+		else if (state == 6 && s[charpos] == 'j')
+			state = 1;
+		else if (state == 6 && s[charpos] == 'w')
+			state = 1;
+		else if (state == 6 && s[charpos] == 'y')
+			state = 1;
+		else if (state == 6 && s[charpos] == 'z')
+			state = 1;
+		// pair = (b|g|h|k|m|n|p|r)
+		else if (state == 6 && s[charpos] == 'b')
+			state = 2;
+		else if (state == 6 && s[charpos] == 'g')
+			state = 2;
+		else if (state == 6 && s[charpos] == 'h')
+			state = 2;
+		else if (state == 6 && s[charpos] == 'k')
+			state = 2;
+		else if (state == 6 && s[charpos] == 'm')
+			state = 2;
+		else if (state == 6 && s[charpos] == 'n')
+			state = 2;
+		else if (state == 6 && s[charpos] == 'p')
+			state = 2;
+		else if (state == 6 && s[charpos] == 'r')
+			state = 2;
+		// s path = s
+		else if (state == 6 && s[charpos] == 's')
+			state = 3;
+		// t path = t
+		else if (state == 6 && s[charpos] == 't')
+			state = 4;
+		// c path = c
+		else if (state == 6 && s[charpos] == 'c')
+			state = 5;
+		else
+			return ERROR;
+		charpos++;
+	}//end of while
 
-  // where did I end up????
-  if ((state == 0) || (state == 6))
-	{ return(true); }  // end in a final state q0 (0) or q0' (6)
-   else return(false);
+	 // where did I end up????
+	if (state == 0)
+	{
+		return WORD1;
+	}
+	else if (state == 6)  // end in a final state q0 (0) or q0' (6)
+	{
+		return WORD2;
+	}
+	else
+		return ERROR;
 }
 
 // ** Add the PERIOD DFA here
@@ -232,48 +248,127 @@ bool period(string s)
 		// logic to say that if the char is a period and there is no next char
 		if ((s[charpos] == '.') && (s[charpos + 1] == '\0'))
 		{
-			return true;
+			return PERIOD;
 		}
 		charpos++;
 	}
-	return false;
+	return ERROR;
 }
 // -----  Tables -------------------------------------
 
 // ** Update the tokentype to be WORD1, WORD2, PERIOD, ERROR, etc.
 // Feel free to add a tokentype for the end-of-file marker.
-enum tokentype {ERROR, WORD1, WORD2, PERIOD, VERB, VERBNEG, VERBPAST, VERBPASTNEG, IS, WAS, OBJECT, SUBJECT, DESTINATION, PRONOUN, CONNECTOR, EOFM};
 
-// ** string tokenName[30] = { }; for the display names of tokens
+string tokenName[30] = { "ERROR", "WORD1", "WORD2", "PERIOD", "VERB", "VERBNEG", "VERBPAST", "VERBPASTNEG", "IS", "WAS", "OBJECT", "SUBJECT", "DESTINATION", "PRONOUN", "CONNECTOR", "EOFM" }; //for the display names of tokens
 
-// ** Need the reservedwords table to be set up here. 
-// ** Do not require any file input for this.
-// ** a.out should work without any additional files.
-// these are the hard coded reserved words as well as their token types
-// these use the pair data structure to have a string in the first slot and a token type in the second
-// a vector of pairs holds them all and is returned by the function
-// note about pairs: they use pairname.first or pairname.second to access elements in them
-vector<pair<string, tokentype>> reservedWords()
+																																															   // ** Need the reservedwords table to be set up here. 
+																																															   // ** Do not require any file input for this.
+																																															   // ** a.out should work without any additional files.
+																																															   // these are the hard coded reserved words as well as their token types
+																																															   // these use the pair data structure to have a string in the first slot and a token type in the second
+																																															   // a vector of pairs holds them all and is returned by the function
+																																															   // note about pairs: they use pairname.first or pairname.second to access elements in them
+
+struct reservedWord
 {
-	vector<pair<string, tokentype>> reserved[18];
-	reservedWords[0].make_pair("masu", VERB);
-	reservedWords[1].make_pair("masen", VERBNEG);
-	reservedWords[2].make_pair("mashita", VERBPAST);
-	reservedWords[3].make_pair("masendeshita", VERBPASTNEG);
-	reservedWords[4].make_pair("desu", IS);
-	reservedWords[5].make_pair("deshita", WAS);
-	reservedWords[6].make_pair("o", OBJECT);
-	reservedWords[7].make_pair("wa", SUBJECT);
-	reservedWords[8].make_pair("ni", DESTINATION);
-	reservedWords[9].make_pair("watashi", PRONOUN);
-	reservedWords[10].make_pair("anata", PRONOUN);
-	reservedWords[11].make_pair("kare", PRONOUN);
-	reservedWords[12].make_pair("kanojo", PRONOUN);
-	reservedWords[13].make_pair("sore", PRONOUN);
-	reservedWords[14].make_pair("mata", CONNECTOR);
-	reservedWords[15].make_pair("soshite", CONNECTOR);
-	reservedWords[16].make_pair("shikashi", CONNECTOR);
-	reservedWords[17].make_pair("dakara", CONNECTOR);
-	reservedWords[18].make_pair("eofm", EOFM);
-	return reserved;
+	string WORD;
+	tokentype TYPE;
+};
+
+vector <reservedWord> reservedWordsList;
+
+void init()
+{
+	reservedWord r;
+	r.WORD = "masu";
+	r.TYPE = VERB;
+	reservedWordsList.push_back(r);
+
+	r.WORD = "masen";
+	r.TYPE = VERBNEG;
+	reservedWordsList.push_back(r);
+
+	r.WORD = "mashita";
+	r.TYPE = VERBPAST;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "masendeshita";
+	r.TYPE = VERBPASTNEG;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "desu";
+	r.TYPE = IS;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "deshita";
+	r.TYPE = WAS;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "o";
+	r.TYPE = OBJECT;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "wa";
+	r.TYPE = SUBJECT;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "ni";
+	r.TYPE = DESTINATION;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "watashi";
+	r.TYPE = PRONOUN;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "anata";
+	r.TYPE = PRONOUN;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "kare";
+	r.TYPE = PRONOUN;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "kanojo";
+	r.TYPE = PRONOUN;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "sore";
+	r.TYPE = PRONOUN;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "mata";
+	r.TYPE = CONNECTOR;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "soshite";
+	r.TYPE = CONNECTOR;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "shikashi";
+	r.TYPE = CONNECTOR;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "dakara";
+	r.TYPE = CONNECTOR;
+	reservedWordsList.push_back(r);
+
+
+	r.WORD = "eofm";
+	r.TYPE = EOFM;
+	reservedWordsList.push_back(r);
 }
