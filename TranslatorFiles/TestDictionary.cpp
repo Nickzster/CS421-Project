eae4d7045ffc773c5d7375e8e95eca904050b271
Nick <nicholas.zimmermann@gmail.com>
2018-11-27 16:47:48 -0800

    Finished Dictionary for translator section

---
+++ TranslatorFiles/TestDictionary.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+#include "VocabTranslator.h"
+
+using namespace std;
+
+int main()
+{
+    VocabTranslator V;
+    V.printAll();
+    return 0;
+}
---
+++ TranslatorFiles/VocabTranslator.h
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+#ifndef TRANSLATOR
+#define TRANSLATOR
+
+struct WORDS
+{
+    string EnglishWord;
+    string GermanWord;
+};
+
+struct VERBS
+{
+    string EnglishVerb;
+    vector <string> GermanConjugations;
+};
+class VocabTranslator
+{
+    private:
+        vector <WORDS> Words;
+        vector <VERBS> Verbs;
+        //vector <string list> germanVerbs;
+
+        int searchForAWord(string);
+        // int searchForAVerb(string);
+        // int searchForASpecificConjugation(string, vector <string> &GermanConjugations);
+        // void displayConjugations(VERBS);
+    public:
+
+    VocabTranslator();
+    ~VocabTranslator();
+    void write();
+    void read();
+    // void addWord(string, string);
+    // void addConjugation(string, vector <string> &GermanConjugations);
+    // //void addWord(string, list);
+    void printAll();
+    string translate(string);
+    // void correction(string,string);
+};
+
+#endif
