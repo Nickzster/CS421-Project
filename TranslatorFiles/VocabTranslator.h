3e2f18b9e5c06876631e53813d680b8034316b1c
Nick Murrin <36419046+murri005@users.noreply.github.com>
2018-12-01 10:51:30 -0800

    Changed both the header and the actual class
    I made it say JapaneseWord instead of GermanWord to make it more relevant to our project at hand.

--- TranslatorFiles/VocabTranslator.h
+++ TranslatorFiles/VocabTranslator.h
@@ -1,50 +1,39 @@
 /*
 VOCABTRANSLATOR HEADER FILE
 VOCAB TRANSLATOR WRITTEN BY NICK ZIMMERMANN.
-Note: Some words are in german / say german... I designed this VocabTranslator to act as a "dictionary" for my german class...
-So I simply made some changes it to do japanese instead.
+Note: Some words are in german / say german... I designed this VocabTranslator to act as a "dictionary" for my German class...
+So I simply made some changes it to do Japanese instead.
 */
-
 #include <iostream>
 #include <list>
 #include <string>
 #include <vector>
-
 using std::string;
 using std::vector;
-
 #ifndef TRANSLATOR
 #define TRANSLATOR
-
 struct WORDS
 {
     string EnglishWord;
-    string GermanWord;
+    string JapaneseWord;
 };
-
 struct VERBS
 {
     string EnglishVerb;
-    vector <string> GermanConjugations;
+    vector <string> JapaneseConjugations;
 };
 class VocabTranslator
 {
     private:
         vector <WORDS> Words;
         vector <VERBS> Verbs;
-
         int searchForAWord(string);
-
     public:
-
     VocabTranslator();
     ~VocabTranslator();
     void write();
     void read();
-
     void printAll();
     string translate(string);
-
 };
-
 #endif
